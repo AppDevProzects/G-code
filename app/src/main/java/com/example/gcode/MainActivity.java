56d8d18876c59802f1be00362ceb456ec409f69e
bhavya2921 <bhavyagra3008@gmail.com>
2021-10-29 17:34:09 +0530

    profile activity layout completed

--- app/src/main/java/com/example/gcode/MainActivity.java
+++ app/src/main/java/com/example/gcode/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button button,profileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +17,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
+        profileButton = findViewById(R.id.profileButton);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
                 startActivity(intent);
             }
         });
